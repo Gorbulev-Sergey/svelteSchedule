1a0eb17c52bee7033983b5fa48c83c1ab36a9f88
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-10-16 20:31:23 +0300

    2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <div class="table-responsive border-0 bg-light" style="border-radius:.25rem">
 	<div class="pl-3 pt-2 pr-3 pb-1">
-		<h6 class="text-dark m-0 p-0 text-nowrap flex-nowrap">
-			<!--!-->Расписание на ОКТЯБРЬ 2022<!--!--> года
-		</h6>
+		<h6 class="text-dark m-0 p-0 text-nowrap flex-nowrap">Расписание на ОКТЯБРЬ 2022 года</h6>
 	</div>
-	<!--!-->
+
 	<table class="table table-borderless bg-light text-dark m-0">
-		<!--!--><thead class="no-print"
+		<thead class="no-print"
 			><tr class="text-light"
 				><th scope="col" class="mr-1"><span class="badge badge-secondary">Дата</span></th>
 				<th scope="col"><span class="badge badge-secondary">Праздник</span></th>
@@ -15,94 +13,94 @@
 			></thead
 		>
 		<tbody class="border-0"
-			><!--!--><tr
+			><tr
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">1 октября, суббота</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="">Преподобной Евфросинии Суздальской</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, панихида</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
-			><!--!--><tr
-				><!--!--><th scope="row" width="16%" class="border-top-0" />
+			><tr
+				><th scope="row" width="16%" class="border-top-0" />
 				<th scope="row" width="60%" class="border-top-0 font-weight-normal"><div class="" /></th><th
 					scope="row"
 					width="20%"
 					class="font-weight-normal text-right"><span class="">Всенощное бдение</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">16:30</div></th
 				></tr
-			><!--!--><tr class="bg-tertiary text-danger"
+			><tr class="bg-tertiary text-danger"
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">2 октября, воскресенье</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="">Мучеников Трофима, Савватия и Дормедонта</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, молебен</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
-			><!--!--><tr
+			><tr
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">4 октября, вторник</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="font-italic">
 						Молебен с акафистом великомученику Георгию Победоносцу, о воинах
 					</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Молебен с акафистом</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">10:00</div></th
 				></tr
-			><!--!--><tr
+			><tr
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">7 октября, пятница</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="">Равноапостольной Феклы Иконийской, первомученицы</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, молебен</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
-			><!--!--><tr
+			><tr
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">8 октября, суббота</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="">
 						Преставление преподобного Сергия, игумена Радонежского, всея России чудотворца
 					</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, молебен</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
-			><!--!--><tr
-				><!--!--><th scope="row" width="16%" class="border-top-0" />
+			><tr
+				><th scope="row" width="16%" class="border-top-0" />
 				<th scope="row" width="60%" class="border-top-0 font-weight-normal"><div class="" /></th><th
 					scope="row"
 					width="20%"
 					class="font-weight-normal text-right"><span class="">Всенощное бдение</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">16:30</div></th
 				></tr
-			><!--!--><tr class="bg-tertiary text-danger"
+			><tr class="bg-tertiary text-danger"
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">9 октября, воскресенье</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="">
 						Преставление Апостола и Евангелиста Иоанна Богослова. Святителя Тихона, патриарха
@@ -110,66 +108,66 @@
 					</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, молебен</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
-			><!--!--><tr
+			><tr
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">11 октября, вторник</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="font-italic">
 						Молебен с акафистом великомученику Георгию Победоносцу, о воинах
 					</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Молебен с акафистом</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">10:00</div></th
 				></tr
-			><!--!--><tr
+			><tr
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">14 октября, пятница</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="text-primary font-weight-bold text-uppercase">
 						Покров Пресвятой Богородицы
 					</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, молебен</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
-			><!--!--><tr
+			><tr
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">15 октября, суббота</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="">
 						Священномученика Киприана и мученицы Иустины, и мученика Феоктиста
 					</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, молебен</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
-			><!--!--><tr
-				><!--!--><th scope="row" width="16%" class="border-top-0" />
+			><tr
+				><th scope="row" width="16%" class="border-top-0" />
 				<th scope="row" width="60%" class="border-top-0 font-weight-normal"><div class="" /></th><th
 					scope="row"
 					width="20%"
 					class="font-weight-normal text-right"><span class="">Всенощное бдение</span></th
-				><!--!-->
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">16:30</div></th
 				></tr
-			><!--!--><tr class="bg-tertiary text-danger"
+			><tr class="bg-tertiary text-danger"
 				><th scope="row" width="16%" class="mr-1 font-weight-normal"
 					><span class="">16 октября, воскресенье</span></th
-				><!--!-->
+				>
 				<th scope="row" width="59%" class="font-weight-normal"
 					><div class="">
 						Священномучеников Дионисия Ареопагита, епископа Афинского, Рустика пресвитера, и
@@ -177,7 +175,67 @@
 					</div></th
 				><th scope="row" width="20%" class="font-weight-normal text-right"
 					><span class="">Литургия, молебен</span></th
-				><!--!-->
+				>
+				<th scope="row" width="4%" class="font-weight-normal"
+					><div class="text-right mr-1">9:00</div></th
+				></tr
+			><tr
+				><th scope="row" width="16%" class="mr-1 font-weight-normal"
+					><span class="">18 октября, вторник</span></th
+				>
+				<th scope="row" width="59%" class="font-weight-normal"
+					><div class="font-italic">Молебен с акафистом Архангелу Михаилу, о воинах</div></th
+				><th scope="row" width="20%" class="font-weight-normal text-right"
+					><span class="">Молебен с акафистом</span></th
+				>
+				<th scope="row" width="4%" class="font-weight-normal"
+					><div class="text-right mr-1">10:00</div></th
+				></tr
+			><tr
+				><th scope="row" width="16%" class="mr-1 font-weight-normal"
+					><span class="">21 октября, пятница</span></th
+				>
+				<th scope="row" width="59%" class="font-weight-normal"
+					><div class="">
+						Преподобного Досифея Верхнеостровского, Псковского. Преподобного Трифона Вятского
+					</div></th
+				><th scope="row" width="20%" class="font-weight-normal text-right"
+					><span class="">Литургия, молебен</span></th
+				>
+				<th scope="row" width="4%" class="font-weight-normal"
+					><div class="text-right mr-1">9:00</div></th
+				></tr
+			><tr
+				><th scope="row" width="16%" class="mr-1 font-weight-normal"
+					><span class="">22 октября, суббота</span></th
+				>
+				<th scope="row" width="59%" class="font-weight-normal"
+					><div class="">Апостола Иакова Алфеева</div></th
+				><th scope="row" width="20%" class="font-weight-normal text-right"
+					><span class="">Литургия, молебен</span></th
+				>
+				<th scope="row" width="4%" class="font-weight-normal"
+					><div class="text-right mr-1">9:00</div></th
+				></tr
+			><tr
+				><th scope="row" width="16%" class="border-top-0" />
+				<th scope="row" width="60%" class="border-top-0 font-weight-normal"><div class="" /></th><th
+					scope="row"
+					width="20%"
+					class="font-weight-normal text-right"><span class="">Всенощное бдение</span></th
+				>
+				<th scope="row" width="4%" class="font-weight-normal"
+					><div class="text-right mr-1">16:30</div></th
+				></tr
+			><tr class="bg-tertiary text-danger"
+				><th scope="row" width="16%" class="mr-1 font-weight-normal"
+					><span class="">23 октября, воскресенье</span></th
+				>
+				<th scope="row" width="59%" class="font-weight-normal"
+					><div class="">Преподобного Амвросия Оптинского</div></th
+				><th scope="row" width="20%" class="font-weight-normal text-right"
+					><span class="">Литургия, молебен</span></th
+				>
 				<th scope="row" width="4%" class="font-weight-normal"
 					><div class="text-right mr-1">9:00</div></th
 				></tr
